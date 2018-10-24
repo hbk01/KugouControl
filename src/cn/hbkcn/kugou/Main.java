223018a12d242a6c6946ab199c65c0e2d310cc58
hbk <3243430237@qq.com>
2018-10-24 16:42:50 +0800

    新增一行空格

--- src/cn/hbkcn/kugou/Main.java
+++ src/cn/hbkcn/kugou/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static final String ip = "127.0.0.1";
     public static final int port = 10201;
 
+
     public static void main(String[] args) {
         receive();
     }
